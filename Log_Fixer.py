55f04c2446b981c440e5be4d77e36a140b0f24a3
evogytis <g.dudas@sms.ed.ac.uk>
2013-01-23 13:39:58 +0100

    Complete rewrite.
    LogFixer can now detect incomplete states as well as missing states, can combine just log files (i.e. ignore tree files) and can deal with arbitrary numbers of files. Dependency on tKinter shouldn't be a problem, because it should come with python 2.7 or later.

--- Log_Fixer.py
+++ Log_Fixer.py
@@ -1,264 +1,147 @@
 import csv
 import re
 import os
+import Tkinter, tkFileDialog
 
-#specify files that need to be analysed for continuity
-#gmrf and rates files are what I usually need
-#if you only have log and tree files leave gmrffile and ratesfile as empty strings i.e. ''
+def index(data,item):
+    return [i for i,x in enumerate(data) if x == item]
 
-logfile = '/Users/admin/Documents/Viral sequences/0311 Influenza/HA Yamagata/HA0411Yamagata_1.log.txt'
-treefile = '/Users/admin/Documents/Viral sequences/0311 Influenza/HA Yamagata/HA0411Yamagata_1.trees.txt'
-gmrffile = '/Users/admin/Documents/Viral sequences/0311 Influenza/HA Yamagata/HA0411Yamagata_1.gmrf.txt'
-ratesfile = '/Users/admin/Documents/Viral sequences/0311 Influenza/HA Yamagata/HA0411Yamagata_1.locations.rates.log'
+choosing=False
+usingTrees=False
 
-#opens log and tree files
-log = csv.reader(open(logfile,'rU'),delimiter='\t',quotechar='"')
-trees = open(treefile,'r')
-
-#open gmrf and rates files if they exist
-if gmrffile != '':
-    gmrf = csv.reader(open(gmrffile,'r'),delimiter='\t',quotechar='"')
-if ratesfile != '':
-    rates = csv.reader(open(ratesfile,'r'),delimiter='\t',quotechar='"')
-
-#keeps track of non-tree files (file formatting reasons)
-allfiles = [logfile,gmrffile,ratesfile]
-
-print '\nNumber of files to be analysed for continuity: %s'%(len(allfiles)-allfiles.count('')+1)
-
-#helps later on
-filenames = ['log','tree','gmrf','rate']
-
-#important bit - rereads data into a different file in case the file contains NULL bytes
-for files in allfiles:
-    if files != '':
-        fi = open(files, 'rb')
-        data = fi.read()
-        fi.close()
-        fo = open(files+'2.txt', 'wb')
-        fo.write(data.replace('\x00', ''))
-        fo.close()
-
-#empty lists where the states from files will be saved
-logstates = []
-treestates = []
-gmrfstates = []
-ratestates = []
-
-#list of lists with states
-statesfiles = [logstates,treestates,gmrfstates,ratestates]
-
-#appends states from the log file to a list
-log2 = csv.reader(open(str(logfile)+'2.txt', 'r'),delimiter='\t',quotechar='"')
-for unit in log2:
-    if 'a' in unit[0]:
-        pass
-    else:
-        L = re.search("[0-9]+",unit[0])
-        if L is not None:
-            logstates.append(int(L.group()))
-
-print '\nStates from the log file are loaded'
-
-#appends states from the tree file to a list
-for unit in trees:
-    P = re.search("tree STATE_[0-9]+",unit)
-    if P is not None:
-        treestates.append(int(P.group()[11:]))
-
-print 'States from the tree file are loaded'
-
-#if gmrf and rates file exist, appends their states to lists
-if gmrffile != '':
-    gmrf2 = csv.reader(open(str(gmrffile)+'2.txt', 'r'),delimiter='\t',quotechar='"')
-    for unit in gmrf2:
-        if 'a' in unit[0]:
-            pass
-        else:
-            G = re.search("[0-9]+",unit[0])
-            if G is not None:
-                gmrfstates.append(int(G.group()))
-    print 'States from the GMRF file are loaded'
-
-if ratesfile != '':
-    rates2 = csv.reader(open(str(ratesfile)+'2.txt', 'r'),delimiter='\t',quotechar='"')
-    for unit in rates2:
-        if 'a' in unit[0]:
-            pass
-        else:
-            R = re.search("[0-9]+",unit[0])
-            if R is not None:
-                ratestates.append(int(R.group()))
-    print 'States from the rates file are loaded'
-
-#this is a list to which states present in all files will be appended
-continuous = []
-
-#determines which function to use based on the number of files to be analysed
-if len(allfiles)-allfiles.count('')+1 == 2:
-    def intersect(a, b):
-        return list(set(a) & set(b))
-    
-if len(allfiles)-allfiles.count('')+1 == 3:
-    def intersect(a,b,c):
-        return list(set(a) & set(b) & set(c))
-
-if len(allfiles)-allfiles.count('')+1 == 4:
-    def intersect(a, b, c, d):
-     return list(set(a) & set(b) & set(c) & set(d))
-
-#inters is a list containing sorted states that are common between all files
-if len(allfiles)-allfiles.count('')+1 == 2:
-    inters = sorted(intersect(logstates,treestates))
-
-if len(allfiles)-allfiles.count('')+1 == 3:
-    if gmrffile !='':
-        inters = sorted(intersect(logstates,treestates,gmrfstates))
-    if ratesfile !='':
-        inters = sorted(intersect(logstates,treestates,ratestates))
-
-if len(allfiles)-allfiles.count('')+1 == 4:
-    inters = sorted(intersect(logstates,treestates,gmrfstates,ratestates))
-
-
-#determines subsampling frequency
-subsample = inters[1]-inters[0]
-
-print '\n'
-
-#some stats about the files
-for files,states in zip(filenames,statesfiles):
-    if len(states) != 0:
-        print 'Number of states in %s file: %s'%(files,len(states))
-
-print '\nStates shared by all files:'
-for unit in range(0,len(inters)):
-    if len(inters) < 50:
-        print '%s\t%s'%(unit+1,inters[unit])
-    else:
-        print '%s'%(len(inters))
-        break
-
-print '\nSubsampling frequency: %s\n'%(subsample)
-
-#statesfiles contains all the states in each file
-#if all files share the same numbers of states then len(status) == sum(status)
-status = []
-for states in statesfiles:
-    if len(states) != 0:
-        if len(inters) == len(states):
-            status.append(1)
-        else:
-            status.append(0)
-
-#if states do not completely overlap between all files this makes new files
-#with cleaned and renumbered states
-if len(status) != sum(status):
-    #collects states common to all files
-    currstate=-subsample
-    for i in range(0,len(inters)-1):
-        if int(inters[i]) != (currstate+subsample):
-            if inters[i-1]+subsample == inters[i]-subsample:
-                print '\nState %s is missing'%(inters[i-1]+subsample)
-                continuous.append(str(inters[i]))
-                currstate=inters[i]
-            else:
-                print '\nStates %s to %s (%s in total) are missing\n'%(inters[i-1]+subsample,inters[i]-subsample,(((inters[i]-subsample)-(inters[i-1]+subsample))/subsample))
-                continuous.append(str(inters[i]))
-                currstate=inters[i]
-        else:
-            continuous.append(str(inters[i]))
-            currstate=inters[i]
-
-
-    #opens temporary files created previously
-    logtemp = csv.reader(open(logfile+'2.txt','rb'),delimiter='\t',quotechar='"')
-    treestemp = open(treefile,'r')
-    
-    if gmrffile != '':
-        gmrftemp = csv.reader(open(gmrffile+'2.txt','r'),delimiter='\t',quotechar='"')
-        
-    if ratesfile != '':
-        ratestemp = csv.reader(open(ratesfile+'2.txt','r'),delimiter='\t',quotechar='"')
-
-    #creates corrected files
-    newlog = open(str(logfile)+'corrected.txt','w')
-    newtrees = open(str(treefile)+'corrected.txt','w')
-
-    if gmrffile != '':
-        newgmrf = open(str(gmrffile)+'corrected.txt','w+')
-    
-    if ratesfile != '':
-        newrates = open(str(ratesfile)+'corrected.txt','w+')
-
-    #print renumbered states and values to file only if they are shared between all files
-    logcounter=-1
-    for unit in logtemp:
-        if 'a' in unit[0]:
-            newlog.write('\n')
-            newlog.write('\t'.join(unit))
-        elif unit[0] in continuous:
-            del unit[0]
-            logcounter+=1
-            newlog.write('\n%s\t'%(logcounter))
-            newlog.write('\t'.join(unit))
-
-    newlog.close()
-    print 'New log file saved'
-    
-    treescounter=-1
-    for unit in treestemp:
-        P = re.search("tree STATE_[0-9]+",unit)
-        if "tree STATE_" not in unit:
-            newtrees.write(unit)
-        if P is not None:
-            if str(P.group()[11:]) in continuous:
-                treescounter+=1
-                newtrees.write('\ntree STATE_%s\t'%(treescounter))
-                newtrees.write(unit[len(P.group()):])
-    newtrees.write('End;')
-    newtrees.close()
-    print 'New trees file saved'
-
-    if gmrffile != '':
-        newgmrf = open(str(gmrffile)+'corrected.txt','wb')
-        gmrfcounter=-1
-        for unit in gmrftemp:
-            if 'a' in unit[0]:
-                newgmrf.write('\n')
-                newgmrf.write('\t'.join(unit))
-            elif unit[0] in continuous:
-                del unit[0]
-                gmrfcounter+=1
-                newgmrf.write('\n%s\t'%(gmrfcounter))
-                newgmrf.write('\t'.join(unit))
-        newgmrf.close()
-        print 'New gmrf file saved'
-
-    if ratesfile != '':
-        newrates = open(str(ratesfile)+'corrected.txt','wb')
-        ratescounter=-1
-        for unit in ratestemp:
-            if 'a' in unit[0]:
-                newrates.write('\n')
-                newrates.write('\t'.join(unit))
-            elif unit[0] in continuous:
-                del unit[0]
-                ratescounter+=1
-                newrates.write('\n%s\t'%(ratescounter))
-                newrates.write('\t'.join(unit))
-        newrates.close()
-        print 'New rates file saved'
-
+root = Tkinter.Tk()
+file = tkFileDialog.askopenfile(parent=root,mode='rb',title='Choose a tree file')
+if file != None:
+        trees=file
+        usingTrees=True
+        print 'Tree file:\n%s'%(file.name.split('/')[-1])
 else:
-    print 'All files already have the same numbers of states!\n'
+    print 'Not checking tree file for consistency.'
+root.withdraw()
 
-print '\n'
+## contains references to the logfiles to be analysed
+logfileList=[]
 
-#removes temporary files
-for files in allfiles:
-    if files != '':
-        print 'Temporary file %s2.txt removed'%(files)
-        os.remove('%s2.txt'%(files))
+## contains state numbers from all the files
+logfileStates=[]
 
-print '\nDone!\n'
+## matrix to check how many columns there should be vs how many there are
+logfileColLength=[]
+
+## loop as long as there are files, proceed if no files left
+while choosing==False:
+    root = Tkinter.Tk()
+    file = tkFileDialog.askopenfile(parent=root,mode='rb',title='Choose a file')
+    if file != None:
+        logfileList.append(file)
+        print '\nFile:\n%s'%(file.name.split('/')[-1])
+    else:
+        choosing=True
+    root.withdraw()
+
+## loads state numbers from tree file, closes it
+if usingTrees==True:
+    logfileStates.append([])
+    treefile=open(trees.name,'r')
+    for i in treefile:
+        cerberus=re.search("tree STATE_([0-9]+)",i)
+        if cerberus is not None:
+            logfileStates[0].insert(0,cerberus.group(1))
+    treefile.close()
+
+## rewrite every file in case of null bytes
+for i in logfileList:
+    logfileStates.append([])
+    logfileColLength.append([])
+    fi = open(i.name, 'rb')
+    data = fi.read()
+    fi.close()
+    fo = open(i.name+'2.txt', 'wb')
+    fo.write(data.replace('\x00', ''))
+    fo.close()
+
+
+## collect state numbers from all files
+for i in range(len(logfileList)):
+    print '\nCollecting states from file %s'%(logfileList[i].name)
+    j=csv.reader(open(logfileList[i].name+'2.txt','r'),delimiter='\t',quotechar='"')
+    for k in j:
+        ## first line doesn't have any useful info
+        if len(k)==1:
+            pass
+        else:
+            colnum=len(k)
+            ## finds the column length
+            if len(logfileColLength[i])==0:
+                #print '\nExpected number of columns for file \n%s: %s'%(logfileList[i].name,colnum)
+                logfileColLength[i].insert(0,colnum)
+            ## only includes states where the full column complement is present
+            if colnum==logfileColLength[i][0]:
+                if usingTrees==True:
+                    logfileStates[i+1].insert(0,k[0])
+                else:
+                    logfileStates[i].insert(0,k[0])
+            else:
+                pass
+                #print 'State %s from file %s was missing %s columns'%(k[0],logfileList[i].name,colnum-logfileColLength[i][0])
+
+## finds the states shared by all files
+common = set(logfileStates[0])
+for i in range(len(logfileStates[1:])):
+    common.intersection(logfileStates[i])
+
+print '\nNumber of states shared by all files: %s'%(len(common))
+
+
+## writes only complete states that are shared by all files
+for i in range(len(logfileList)):
+    #print '\nWriting shared states from file %s'%(logfileList[i].name)
+    with open(logfileList[i].name+'2.txt') as f:
+        j=csv.reader(f,delimiter='\t')
+        out=open(logfileList[i].name.replace('.txt','.fixed.txt'),'w+')
+        linecounter=-1
+        for k in j:
+            if len(k)==1:
+                print>>out,k[0]
+            else:
+                if k[0]=='state':
+                    string=''
+                    for l in k[:-1]:
+                        string+=l+'\t'
+                    string+=k[-1]
+                    print>>out,string
+                elif k[0] in common:
+                    string=str(linecounter)+'\t'
+                    for l in k[1:-1]:
+                        string+=l+'\t'
+                    string+=k[-1]
+                    print>>out,string
+                linecounter+=1
+        out.close()
+
+
+## writes trees which are associated with complete states shared by all files
+if usingTrees==True:
+    treescounter=-1
+    trees=open(trees.name,'r')
+    out=open(trees.name.replace('.txt','.fixed.txt'),'w+')
+    for i in trees:
+        cerberus=re.search("tree STATE_([0-9]+)",i)
+        if "tree STATE_" not in i:
+            print>>out,i.strip('\n')
+        if cerberus is not None:
+            if str(cerberus.group(1)) in common:
+                print>>out,'tree STATE_%s\t%s'%(treescounter,i[len(cerberus.group()):])
+                treescounter+=1
+    ## finishing touches
+    out.write('End;')
+    out.close()
+
+
+## cleanup - remove temporary files used for removing null bytes
+for i in logfileList:
+    os.remove('%s2.txt'%(i.name))
+    #print '\nTemporary file %s2.txt removed'%(i.name)
+
+
+print '\nDone!'
